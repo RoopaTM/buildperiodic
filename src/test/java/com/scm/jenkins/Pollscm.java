6782004ca30871b3f2ab45962a8760056c28dbaf
Roopa <Roopa@ROOPA>
2022-05-30 23:43:39 -0700

    commited

---
+++ src/test/java/com/scm/jenkins/Pollscm.java
@@ -0,0 +1,13 @@
+package com.scm.jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Pollscm 
+{
+	@Test
+	 public void teststep1Test()
+	 {
+		 Reporter.log("test case pass");
+	 }
+}
